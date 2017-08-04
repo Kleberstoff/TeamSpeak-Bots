02466d160a981e9f48c6f4cc5171498c394f66ca
DatDraggy <bmxstreet@gmx.net>
2017-08-04 10:37:30 +0200

    Missing configs
    Had config file of updater, fixed missing configs

--- Auto_Sub_Channel_Creator/libraries/config.php
+++ Auto_Sub_Channel_Creator/libraries/config.php
@@ -6,6 +6,7 @@ require("TeamSpeak3/TeamSpeak3.php");
 /* Config Start with Array */
 $config = array();
 
+/* Give the Array Information */
 $config['Username'] = "test"; // Login name for the Query
 $config['Password'] = "XK09HBXj"; // Password for the Query
 $config['serverIP'] = "127.0.0.1"; // Server IP or Domain Name
@@ -13,13 +14,12 @@ $config['sPort'] = "9987"; // Server Port for the Query | Default: 9987
 $config['qPort'] = "10011"; // Query Port for the Query | Default: 10011
 $config['BotName'] = rawurlencode("I am normally invisible"); // url encoded bot name
 $config['CheckDelay'] = 1; // Amount of Seconds between each Check. Only use Number greater then 1. Faster Checks are useful for bigger TeamSpeaks, in smaller TeamSpeaks, this can be higher
-
 /* Bot Config End */
 
 /* TeamSpeak Settings */
-
-$config['TimeChannel'] = "3";
-$config['UserChannel'] = "4";
+$config['PublicChannels'] = array(3, 4, 5); // Put the Public Channels here | Note: The Last Channel in this List should always be the last Channel in the TeamSpeak Order as well
+$config['TempChannelName'] = "Temp. Public Channel "; // Temporary Public Channel Name
+$config['TempMaxClients'] = 2; // Set the Max Clients for new Temp Channels
 $config['ChannelPermissions'] = array(
   /* Example permissions:
    * "i_channel_needed_subscribe_power=500",
